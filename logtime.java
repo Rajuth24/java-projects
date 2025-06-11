e89555a777f5e659e21cd58fc9208a85ea704405
Rajuth24 <thraju24@gmail.com>
2025-06-11 12:22:49 +0530

    Add files via upload

---
+++ logtime.java
@@ -0,0 +1,28 @@
+package demo;
+
+public class logtime {
+
+	public static void main(String[] args) {
+		int[] numbers = {1,3,5,7,11,13};
+		int target = 11;
+		int low =0;
+		
+		int high = numbers.length - 1;
+		while (low<=high) {
+			int mid = (low +high)/2;
+			if(numbers[mid]==target) {
+				System.out.println("found number" + target + " at position"+ mid);
+				return;
+				
+			}else if (numbers[mid]<target) {
+				low=mid +1;
+			}else {
+				high = mid - 1;
+				
+			}
+		}
+		System.out.println("number not found.");
+
+	}
+
+}
