5f128278f038d7f451054b1431c3a2b8251e6b5e
Rajuth24 <thraju24@gmail.com>
2025-06-11 12:27:43 +0530

    Add files via upload

---
+++ stringreverse.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class stringreverse {
+
+	public static void main(String[] args) {
+		String original=" MS Dhoni";
+		String reverse=new StringBuilder(original).reverse().toString();
+		System.out.println(reverse);
+	
+
+	}
+
+}
